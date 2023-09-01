ffa5d32f220cc43fc89a27b3cd20b139f4cc1d6f
junyahaga <n0812034@yahoo.co.jp>
2023-09-02 00:50:26 +0900

    2023/9/2

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+    //ハンバーガーメニュー
+    $('.js-hamburger').on('click', function () {
+        if ($('.js-hamburger').hasClass('is-open')) {
+            $('.js-drawer-menu').removeClass("is-open");
+            $(this).removeClass('is-open');
+        } else {
+            $('.js-drawer-menu').addClass("is-open");
+            $(this).addClass('is-open');
+        }
+    });
 
+    
 
 });
